ca80d7c45eb73bb1d053bd5bd994b4a09898c8ee
mmhmddd <mohamed.m.mahmoud@gmail.com>
2025-06-27 21:08:47 +0300

    finish 2 services

---
+++ js/Content writing.js
@@ -0,0 +1,14 @@
+// Navigation for Hero Section button
+document.querySelector('.btn-contact-hero').addEventListener('click', function() {
+  window.location.href = '../contact.html';
+});
+
+// Navigation for CTA button
+document.querySelector('.cta .btn-large').addEventListener('click', function() {
+  window.location.href = '../contact.html';
+});
+
+// Navigation for View All Articles button
+document.querySelector('.view-all-articles .btn').addEventListener('click', function() {
+  window.location.href = '../articles/index.html';
+});
